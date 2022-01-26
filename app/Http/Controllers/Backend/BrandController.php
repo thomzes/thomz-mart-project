f2705f0ff1ac9a10722896e169c6496aa5ac2a41
Thomzes <thomz161@gmail.com>
2022-01-27 01:23:43 +0700

    Add brand_edit, route, method BrandUpdate

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -48,5 +48,60 @@ class BrandController extends Controller
 
     }
 
+    public function BrandEdit($id)
+    {
+        $brand = Brand::findOrFail($id);
+        return view('backend.brand.brand_edit', compact('brand'));
+    }
+
+    public function BrandUpdate(Request $request)
+    {
+        $brand_id = $request->id;
+        $old_image = $request->old_image;
+
+        if ($request->file('brand_image')) {
+
+            unlink($old_image);
+            $image = $request->file('brand_image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
+            $save_url = 'upload/brand/'.$name_gen;
+            
+            Brand::findOrFail($brand_id)->update(
+            [
+                'brand_name_en' => $request->brand_name_en,
+                'brand_name_idn' => $request->brand_name_idn,
+                'brand_slug_en' => strtolower(str_replace(' ','-',$request->brand_name_en)),
+                'brand_slug_idn' => strtolower(str_replace(' ','-',$request->brand_name_idn)),
+                'brand_image' => $save_url,
+            ]);
+    
+            // Notification Toastr
+            $notification = array(
+                'message' => 'Brand Updated Successfully!',
+                'alert-type' => 'info'
+            );
+    
+            return redirect()->route('all.brand')->with($notification);
+        }else {
+            Brand::findOrFail($brand_id)->update(
+                [
+                    'brand_name_en' => $request->brand_name_en,
+                    'brand_name_idn' => $request->brand_name_idn,
+                    'brand_slug_en' => strtolower(str_replace(' ','-',$request->brand_name_en)),
+                    'brand_slug_idn' => strtolower(str_replace(' ','-',$request->brand_name_idn)),
+                ]);
+        
+                // Notification Toastr
+                $notification = array(
+                    'message' => 'Brand Updated Successfully!',
+                    'alert-type' => 'info'
+                );
+        
+                return redirect()->route('all.brand')->with($notification);
+        }
+        
+    }
+
 
 }
--- resources/views/backend/brand/brand_edit.blade.php
+++ resources/views/backend/brand/brand_edit.blade.php
@@ -7,62 +7,26 @@
     <!-- Main content -->
     <section class="content">
       <div class="row"> 
-        <div class="col-8">
-         <div class="box">
-            <div class="box-header with-border">
-              <h3 class="box-title">Brand List</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                <div class="table-responsive">
-                  <table id="example1" class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>Brand En</th>
-                            <th>Brand Idn</th>
-                            <th>Image</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($brands as $item)
-                            <tr>
-                                <td>{{ $item->brand_name_en }}</td>
-                                <td>{{ $item->brand_name_idn }}</td>
-                                <td><img src="{{ asset($item->brand_image) }}" style="width: 70px; height:40px;"></td>
-                                <td>
-                                    <a href="{{ route('brand.edit',$item->id) }}" class="btn btn-info">Edit</a>
-                                    <a href="" class="btn btn-danger">Delete</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                        
-                    </tbody>
-                  </table>
-                </div>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-        </div>
-        <!-- /.col -->
-
+        
         {{-- Add Brand Page --}}
-        <div class="col-4">
+        <div class="col-12">
 
             <div class="box">
                <div class="box-header with-border">
-                 <h3 class="box-title">Add Brand</h3>
+                 <h3 class="box-title">Edit Brand</h3>
                </div>
                <!-- /.box-header -->
                <div class="box-body">
                    <div class="table-responsive">
-                    <form method="post" action="{{ route('brand.store') }}" enctype="multipart/form-data">
+                    <form method="post" action="{{ route('brand.update',$brand->id) }}" enctype="multipart/form-data">
                         @csrf
+                        <input type="hidden" name="id" value="{{ $brand->id }}">
+                        <input type="hidden" name="old_image" value="{{ $brand->brand_image }}">
+
                             <div class="form-group">
                                 <h5>Brand Name English<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="brand_name_en" class="form-control">
+                                    <input type="text" name="brand_name_en" class="form-control" value="{{ $brand->brand_name_en }}">
                                     @error('brand_name_en')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -72,7 +36,7 @@
                             <div class="form-group">
                                 <h5>Brand Name Indonesia<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="brand_name_idn" class="form-control">
+                                    <input type="text" name="brand_name_idn" class="form-control" value="{{ $brand->brand_name_idn }}">
                                     @error('brand_name_idn')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -89,7 +53,8 @@
                                 </div>
                             </div>
                            <div class="text-xs-right">
-                               <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add Brand">
+                            <a href="{{ route('all.brand') }}" class="btn btn-rounded btn-warning mb-5">Back</a>
+                               <input type="submit" class="btn btn-rounded btn-primary mb-5 ml-5" value="Update">
                             </div>
                        </form>
                    </div>
