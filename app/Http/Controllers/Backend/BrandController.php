869ab42d3b8dd93e767dae7c5739099d9c40ef2e
Thomzes <thomz161@gmail.com>
2022-01-27 00:47:32 +0700

    Edit the brand_view and method BrandStore

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Models\Brand;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Intervention\Image\Facades\Image;
 
 class BrandController extends Controller
 {
@@ -14,9 +15,37 @@ class BrandController extends Controller
         return view('backend.brand.brand_view', compact('brands'));
     }
 
-    public function BrandStore()
+    public function BrandStore(Request $request)
     {
+        $request->validate(
+        [
+            'brand_name_en' => 'required',
+            'brand_name_idn' => 'required',
+            'brand_image' => 'required', 
+        ]);
+
+        $image = $request->file('brand_image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
+        $save_url = 'upload/brand/'.$name_gen;
         
+        Brand::insert(
+        [
+            'brand_name_en' => $request->brand_name_en,
+            'brand_name_idn' => $request->brand_name_idn,
+            'brand_slug_en' => strtolower(str_replace(' ','-',$request->brand_name_en)),
+            'brand_slug_idn' => strtolower(str_replace(' ','-',$request->brand_name_idn)),
+            'brand_image' => $save_url,
+        ]);
+
+        // Notification Toastr
+        $notification = array(
+            'message' => 'Brand Inserted Successfully!',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
     }
 
 
--- resources/views/backend/brand/brand_view.blade.php
+++ resources/views/backend/brand/brand_view.blade.php
@@ -29,7 +29,7 @@
                             <tr>
                                 <td>{{ $item->brand_name_en }}</td>
                                 <td>{{ $item->brand_name_idn }}</td>
-                                <td><img src="{{ asset('$item->brand_image') }}" style="width: 70px; height:40px;"></td>
+                                <td><img src="{{ asset($item->brand_image) }}" style="width: 70px; height:40px;"></td>
                                 <td>
                                     <a href="" class="btn btn-info">Edit</a>
                                     <a href="" class="btn btn-danger">Delete</a>
@@ -63,6 +63,9 @@
                                 <h5>Brand Name English<span class="text-danger">*</span></h5>
                                 <div class="controls">
                                     <input type="text" name="brand_name_en" class="form-control">
+                                    @error('brand_name_en')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
 
@@ -70,13 +73,19 @@
                                 <h5>Brand Name Indonesia<span class="text-danger">*</span></h5>
                                 <div class="controls">
                                     <input type="text" name="brand_name_idn" class="form-control">
+                                    @error('brand_name_idn')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
 
                             <div class="form-group">
                                 <h5>Brand Image<span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="file" name="brand_image" class="form-control" required>
+                                    <input type="file" name="brand_image" class="form-control">
+                                    @error('brand_image')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
                            <div class="text-xs-right">
