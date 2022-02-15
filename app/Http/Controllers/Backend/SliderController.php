a783afcc741cc0cea58e936c5ecfbbfbe0deb0e8
Thomzes <thomz161@gmail.com>
2022-02-16 01:02:28 +0700

    Add route and method BrandStore

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
 use App\Models\Slider;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Intervention\Image\Facades\Image;
 
 class SliderController extends Controller
 {
@@ -13,7 +14,38 @@ class SliderController extends Controller
         $sliders = Slider::latest()->get();
 
         return view('backend.slider.slider_view', compact('sliders'));
-    }
+
+    } //end method
+
+    public function SliderStore(Request $request)
+    {
+        $request->validate(
+        [
+            'slider_img' => 'required', 
+        ]);
+
+        $image = $request->file('slider_img');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(870,370)->save('upload/slider/'.$name_gen);
+        $save_url = 'upload/slider/'.$name_gen;
+        
+        Slider::insert(
+        [
+            'title' => $request->title,
+            'description' => $request->description,
+            'slider_img' => $save_url,
+
+        ]);
+
+        // Notification Toastr
+        $notification = array(
+            'message' => 'Slider Inserted Successfully!',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    } //end method
 
 
 
