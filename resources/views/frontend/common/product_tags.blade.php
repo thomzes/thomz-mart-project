090dea01162de1937d03caf30edb41b895383803
Thomzes <thomz161@gmail.com>
2022-02-19 12:41:46 +0700

    Product Tags Show

---
+++ resources/views/frontend/common/product_tags.blade.php
@@ -0,0 +1,33 @@
+@php
+    
+    $tags_en = App\Models\Product::groupBy('product_tags_en')->select('product_tags_en')->get();
+    
+    $tags_idn = App\Models\Product::groupBy('product_tags_idn')->select('product_tags_idn')->get();
+
+@endphp
+
+
+
+<div class="sidebar-widget product-tag wow fadeInUp">
+    <h3 class="section-title">Product tags</h3>
+    <div class="sidebar-widget-body outer-top-xs">
+
+      <div class="tag-list">
+
+        @if (session()->get('language') == 'indo')
+            @foreach($tags_idn as $tag)
+            <a class="item active" title="Phone" href="category.html">{{ str_replace(',',' ', $tag->product_tags_idn) }}</a>
+            @endforeach
+        @else
+            @foreach($tags_en as $tag)
+            <a class="item active" title="Phone" href="category.html">{{ str_replace(',',' ', $tag->product_tags_en) }}</a>
+            @endforeach
+        @endif
+
+      </div>
+            
+      <!-- /.tag-list --> 
+    </div>
+    <!-- /.sidebar-widget-body --> 
+  </div>
+  <!-- /.sidebar-widget --> 
