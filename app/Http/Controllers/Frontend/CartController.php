6daf3a8ade8a7d0ee4d7ca1a3c4cd12a187abe83
Thomzes <thomz161@gmail.com>
2022-03-03 22:50:33 +0700

    Wishlist Product Add pt.3

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -86,13 +86,20 @@ class CartController extends Controller
 
             $exists = Wishlist::where('user_id', Auth::id())->where('product_id', $product_id)->first();
 
-            Wishlist::insert([
-                'user_id' => Auth::id(),
-                'product_id' => $product_id,
-                'created_at' => Carbon::now(),
-            ]);
+            if (!$exists) {
+                Wishlist::insert([
+                    'user_id' => Auth::id(),
+                    'product_id' => $product_id,
+                    'created_at' => Carbon::now(),
+                ]);
+    
+                return response()->json(['success' => 'Successfully Added On Your Wishlist!']);
 
-            return response()->json(['success' => 'Successfully Added On Your Wishlist!']);
+            } else {
+                return response()->json(['error' => 'This Product Already On Your Wishlist!']);
+
+            }
+
 
         }else{
 
