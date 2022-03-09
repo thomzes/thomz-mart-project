e69102e161d6063ba048e7fb514a81936fda79a6
Thomzes <thomz161@gmail.com>
2022-03-09 16:43:44 +0700

    Stripe Payment Gateaway | Order Placed Success pt.1

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -2,28 +2,69 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class StripeController extends Controller
 {
     public function StripeOrder(Request $request)
     {
-        
-        \Stripe\Stripe::setApiKey('sk_test_51Kb5isCllP4FZQ2JWLzKsO7FTqKwkXQDod40tDODG7kepXrEIxq5gY5jKDZgVJicl18nvU8rMsNayJmIBtsmO8Rh00zeSIoSXM');
+        if (Session::has('coupon')) {
+            $total_amount = Session::get('coupon')['total_amount'];
+        }else {
+            $total_amount = Cart::total();
+        }
 
+        \Stripe\Stripe::setApiKey('sk_test_51Kb5isCllP4FZQ2JWLzKsO7FTqKwkXQDod40tDODG7kepXrEIxq5gY5jKDZgVJicl18nvU8rMsNayJmIBtsmO8Rh00zeSIoSXM');
         
         $token = $_POST['stripeToken'];
 
         $charge = \Stripe\Charge::create([
-        'amount' => 999*100,
+        'amount' => $total_amount*100,
         'currency' => 'usd',
         'description' => 'Thomz Online Store',
         'source' => $token,
-        'metadata' => ['order_id' => '6735'],
+        'metadata' => ['order_id' => uniqid()],
         ]);
 
-        dd($charge);
+        // dd($charge);
+
+        $order_id = Order::insertGetId([
+            'user_id' => Auth::id(),
+            'division_id' => $request->division_id,
+            'district_id' => $request->district_id,
+            'state_id' => $request->state_id,
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'post_code' => $request->post_code,
+            'notes' => $request->notes,
+
+            'payment_type' => 'Stripe',
+            'payment_method' => 'Stripe',
+            'payment_type' => $charge->payment_method,
+            'transaction_id' => $charge->balance_transaction,
+            'currency' => $charge->currency,
+            'amount' => $total_amount,
+            'order_number' => $charge->metadata->order_id,
+            'invoice_no' => 'TOS'.mt_rand(10000000,99999999),
+            'order_date' => Carbon::now()->format('d F Y'),
+            'order_month' => Carbon::now()->format('F'),
+            'order_year' => Carbon::now()->format('Y'),
+            'status' => 'Pending',
+            'created_at' => Carbon::now(),
+            
+
+
+
+
+
+        ]);
 
 
 
