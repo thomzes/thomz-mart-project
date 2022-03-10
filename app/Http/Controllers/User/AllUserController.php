8036940826e65075d65ce57c5106d59fd4352652
Thomzes <thomz161@gmail.com>
2022-03-10 23:04:36 +0700

    Make Order Invoice & Generate PDF pt.1

--- app/Http/Controllers/User/AllUserController.php
+++ app/Http/Controllers/User/AllUserController.php
@@ -38,6 +38,25 @@ class AllUserController extends Controller
     } //end method
 
 
+    public function InvoiceDownload($order_id)
+    {
+        $order = Order::with('division','district','state','user')
+                        ->where('id', $order_id)
+                        ->where('user_id', Auth::id())
+                        ->first();
+        $orderItem = OrderItem::with('product')
+                                ->where('order_id', $order_id)
+                                ->orderBy('id', 'DESC')
+                                ->get();
+
+        return view('frontend.user.order.order_invoice', compact('order', 'orderItem'));
+
+
+
+
+    } //end method  
+
+
 
 
 }
