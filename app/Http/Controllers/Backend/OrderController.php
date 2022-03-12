33ccabd5c341271a6da70d2afdf03f03a200f501
Thomzes <thomz161@gmail.com>
2022-03-12 18:23:50 +0700

    Read Orders By Status

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -38,6 +38,88 @@ class OrderController extends Controller
 
 
 
+    // Confirmed Order
+    public function ConfirmedOrders()
+    {
+        $orders = Order::where('status', 'Confirmed')
+                        ->orderBy('id', 'DESC')
+                        ->get();
+
+        return view('backend.orders.confirmed_orders', compact('orders'));
+
+    } //end method
+    
+    
+    
+    // Processing Order
+    public function ProcessingOrders()
+    {
+        $orders = Order::where('status', 'Processing')
+                        ->orderBy('id', 'DESC')
+                        ->get();
+
+        return view('backend.orders.processing_orders', compact('orders'));
+
+    } //end method
+    
+    
+    
+    // Picked Order
+    public function PickedOrders()
+    {
+        $orders = Order::where('status', 'Picked')
+                        ->orderBy('id', 'DESC')
+                        ->get();
+
+        return view('backend.orders.picked_orders', compact('orders'));
+
+    } //end method
+    
+
+
+    // Shipped Order
+    public function ShippedOrders()
+    {
+        $orders = Order::where('status', 'Shipped')
+                        ->orderBy('id', 'DESC')
+                        ->get();
+
+        return view('backend.orders.shipped_orders', compact('orders'));
+
+    } //end method
+    
+    
+    
+    // Delivered Order
+    public function DeliveredOrders()
+    {
+        $orders = Order::where('status', 'Delivered')
+                        ->orderBy('id', 'DESC')
+                        ->get();
+
+        return view('backend.orders.delivered_orders', compact('orders'));
+
+    } //end method
+    
+    
+    
+    // Delivered Order
+    public function CancelOrders()
+    {
+        $orders = Order::where('status', 'Cancel')
+                        ->orderBy('id', 'DESC')
+                        ->get();
+
+        return view('backend.orders.cancel_orders', compact('orders'));
+
+    } //end method
+
+
+
+
+
+
+
 
 
 
