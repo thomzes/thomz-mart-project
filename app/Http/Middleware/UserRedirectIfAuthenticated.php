b6b4c79e0e9542e7b9ffd689c575f19a4dcda6fe
Thomzes <thomz161@gmail.com>
2022-03-15 19:28:45 +0700

    Admin Panel Detect Online Offline User

--- app/Http/Middleware/UserRedirectIfAuthenticated.php
+++ app/Http/Middleware/UserRedirectIfAuthenticated.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
+use Carbon\Carbon;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use App\Providers\RouteServiceProvider;
 
 class UserRedirectIfAuthenticated
 {
@@ -19,6 +22,15 @@ class UserRedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
+
+        if(Auth::check()){
+            $expireTime = Carbon::now()->addSeconds(30);
+            Cache::put('user-is-online' . Auth::user()->id, true, $expireTime);
+            User::where('id',Auth::user()->id)->update(['last_seen' => Carbon::now()]);
+        }
+
+
+
 
         if(Auth::check() && Auth::user()) {
             return $next($request);
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Cache;
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -28,6 +29,7 @@ class User extends Authenticatable
         'email',
         'phone',
         'password',
+        'last_seen',
     ];
 
     /**
@@ -59,4 +61,18 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+
+
+    // Show User Active
+    public function UserOnline(){
+        return Cache::has('user-is-online' . $this->id);
+    }
+
+
+
+
+
+
+
 }
