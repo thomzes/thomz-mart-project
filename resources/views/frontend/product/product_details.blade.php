fcdeec0f16a7c8e173ce14597e07a22e0ce7d239
Thomzes <thomz161@gmail.com>
2022-03-30 14:32:25 +0700

    Product Review System pt.1

--- resources/views/frontend/product/product_details.blade.php
+++ resources/views/frontend/product/product_details.blade.php
@@ -8,10 +8,6 @@
 
 
 
-
-
-
-
 <div class="breadcrumb">
 	<div class="container">
 		<div class="breadcrumb-inner">
@@ -329,6 +325,12 @@
 									</div>	
 								</div><!-- /.tab-pane -->
 
+
+
+
+
+								{{-- START CUSTOMER REVIEW --}}
+
 								<div id="review" class="tab-pane">
 									<div class="product-tab">
 																				
@@ -343,90 +345,65 @@
 											
 											</div><!-- /.reviews -->
 										</div><!-- /.product-reviews -->
-										
 
-										
+
 										<div class="product-add-review">
 											<h4 class="title">Write your own review</h4>
 											<div class="review-table">
-												<div class="table-responsive">
-													<table class="table">	
-														<thead>
-															<tr>
-																<th class="cell-label">&nbsp;</th>
-																<th>1 star</th>
-																<th>2 stars</th>
-																<th>3 stars</th>
-																<th>4 stars</th>
-																<th>5 stars</th>
-															</tr>
-														</thead>	
-														<tbody>
-															<tr>
-																<td class="cell-label">Quality</td>
-																<td><input type="radio" name="quality" class="radio" value="1"></td>
-																<td><input type="radio" name="quality" class="radio" value="2"></td>
-																<td><input type="radio" name="quality" class="radio" value="3"></td>
-																<td><input type="radio" name="quality" class="radio" value="4"></td>
-																<td><input type="radio" name="quality" class="radio" value="5"></td>
-															</tr>
-															<tr>
-																<td class="cell-label">Price</td>
-																<td><input type="radio" name="quality" class="radio" value="1"></td>
-																<td><input type="radio" name="quality" class="radio" value="2"></td>
-																<td><input type="radio" name="quality" class="radio" value="3"></td>
-																<td><input type="radio" name="quality" class="radio" value="4"></td>
-																<td><input type="radio" name="quality" class="radio" value="5"></td>
-															</tr>
-															<tr>
-																<td class="cell-label">Value</td>
-																<td><input type="radio" name="quality" class="radio" value="1"></td>
-																<td><input type="radio" name="quality" class="radio" value="2"></td>
-																<td><input type="radio" name="quality" class="radio" value="3"></td>
-																<td><input type="radio" name="quality" class="radio" value="4"></td>
-																<td><input type="radio" name="quality" class="radio" value="5"></td>
-															</tr>
-														</tbody>
-													</table><!-- /.table .table-bordered -->
-												</div><!-- /.table-responsive -->
+
 											</div><!-- /.review-table -->
 											
 											<div class="review-form">
-												<div class="form-container">
-													<form role="form" class="cnt-form">
-														
-														<div class="row">
-															<div class="col-sm-6">
-																<div class="form-group">
-																	<label for="exampleInputName">Your Name <span class="astk">*</span></label>
-																	<input type="text" class="form-control txt" id="exampleInputName" placeholder="">
-																</div><!-- /.form-group -->
-																<div class="form-group">
-																	<label for="exampleInputSummary">Summary <span class="astk">*</span></label>
-																	<input type="text" class="form-control txt" id="exampleInputSummary" placeholder="">
-																</div><!-- /.form-group -->
-															</div>
 
-															<div class="col-md-6">
-																<div class="form-group">
-																	<label for="exampleInputReview">Review <span class="astk">*</span></label>
-																	<textarea class="form-control txt txt-review" id="exampleInputReview" rows="4" placeholder=""></textarea>
-																</div><!-- /.form-group -->
-															</div>
-														</div><!-- /.row -->
-														
-														<div class="action text-right">
-															<button class="btn btn-primary btn-upper">SUBMIT REVIEW</button>
-														</div><!-- /.action -->
+												@guest
+													<p><b>Want to Review The Product? You Need to Login First <a href="{{ route('login') }}">Login Here</a></b></p>
 
-													</form><!-- /.cnt-form -->
-												</div><!-- /.form-container -->
+												@else
+													<div class="form-container">
+														<form role="form" class="cnt-form">
+															
+															<div class="row">
+																<div class="col-sm-6">
+																	<div class="form-group">
+																		<label for="exampleInputSummary">Summary <span class="astk">*</span></label>
+																		<input type="text" class="form-control txt" id="exampleInputSummary" placeholder="">
+																	</div><!-- /.form-group -->
+																</div>
+
+																<div class="col-md-6">
+																	<div class="form-group">
+																		<label for="exampleInputReview">Review <span class="astk">*</span></label>
+																		<textarea class="form-control txt txt-review" id="exampleInputReview" rows="4" placeholder=""></textarea>
+																	</div><!-- /.form-group -->
+																</div>
+															</div><!-- /.row -->
+															
+															<div class="action text-right">
+																<button class="btn btn-primary btn-upper">SUBMIT REVIEW</button>
+															</div><!-- /.action -->
+
+														</form><!-- /.cnt-form -->
+													</div><!-- /.form-container -->
+
+												@endguest
+
+
 											</div><!-- /.review-form -->
 
 										</div><!-- /.product-add-review -->										
 										
 							        </div><!-- /.product-tab -->
 								</div><!-- /.tab-pane -->
+
+								{{-- END CUSTOMER REVIEW --}}
+
+
+
+
+
+
+
+
 
 								<div id="tags" class="tab-pane">
 									<div class="product-tag">
